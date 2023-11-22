7046a2ee39a5a3fa92eca71fe1c89c006b51ff69
huliyoudiangou <75266067+huliyoudiangou@users.noreply.github.com>
2023-11-22 19:40:21 +0800

    Update webBenchmark.sh

--- webBenchmark.sh
+++ webBenchmark.sh
@@ -12,36 +12,84 @@ do
     echo "1. 开始运行 webBenchmark"
     echo "2. 停止运行 webBenchmark"
     echo "3. 退出脚本"
-    
+
     read -p "请输入您的选择（1/2/3）：" choice
-    
+
     case $choice in
         1)
             # 安装 Git 和 Golang
-            yum install git golang || apt install git golang || dnf install screen || pkg install git golang
+            if [[ -x "$(command -v apt-get)" ]]; then
+                sudo apt-get install git golang
+            elif [[ -x "$(command -v yum)" ]]; then
+                sudo yum install git golang
+            elif [[ -x "$(command -v dnf)" ]]; then
+                sudo dnf install git golang
+            elif [[ -x "$(command -v pkg)" ]]; then
+                sudo pkg install git golang
+            else
+                echo "Unsupported package manager."
+                exit 1
+            fi
+
             # 安装 screen 避免任务被系统杀死
-            apt-get install screen || yum install screen || dnf install screen || pkg install screen
+            if [[ -x "$(command -v apt-get)" ]]; then
+                sudo apt-get install screen
+            elif [[ -x "$(command -v yum)" ]]; then
+                sudo yum install screen
+            elif [[ -x "$(command -v dnf)" ]]; then
+                sudo dnf install screen
+            elif [[ -x "$(command -v pkg)" ]]; then
+                sudo pkg install screen
+            else
+                echo "Unsupported package manager."
+                exit 1
+            fi
+
             # 编译架构
             git clone https://github.com/maintell/webBenchmark.git
             cd webBenchmark
             go build
+
             # 赋予权限
             chmod +x webBenchmark
+
             # 提供一个用户键入 url 的界面
             read -p "请输入您要测试的 url：" url
+
             # 直接开搞
             screen -S webBenchmarkSession -dm bash -c "./webBenchmark -c 32 -s $url"
+
             echo "webBenchmark 已经开始运行，您可以按 Ctrl+A+D 退出 screen 窗口任务，或者输入 2 停止运行 webBenchmark"
             ;;
         2)
+            # 安装psmisc
+            if [[ -x "$(command -v apt-get)" ]]; then
+                sudo apt-get install psmisc
+            elif [[ -x "$(command -v yum)" ]]; then
+                sudo yum install psmisc
+            elif [[ -x "$(command -v dnf)" ]]; then
+                sudo dnf install psmisc
+            elif [[ -x "$(command -v pkg)" ]]; then
+                sudo pkg install psmisc
+            else
+                echo "Unsupported package manager."
+                exit 1
+            fi
+
             # 回到 screen 窗口任务
             screen -r webBenchmarkSession
+
+            # 关闭当前 screen 窗口
+            screen -X -S webBenchmarkSession quit
+
             # 获取所有名为"webBenchmark"的进程的PID，并逐个杀死
             pids=$(pgrep -f "webBenchmark")
+
             for pid in $pids
             do
                 killall -9 webBenchmark
             done
+
             echo "webBenchmark 已经停止运行，您可以输入 3 退出脚本"
             ;;
         3)
