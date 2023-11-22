5e46771a2737a3878f0a42f1441769c1e534d9ff
huliyoudiangou <75266067+huliyoudiangou@users.noreply.github.com>
2023-11-22 20:03:09 +0800

    Update webBenchmark.sh

--- webBenchmark.sh
+++ webBenchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 切换到root目录
-cd
+# 保存当前目录
+current_dir=$(pwd)
 
 echo "欢迎使用一键命令脚本，您可以选择以下操作："
 
@@ -17,38 +17,40 @@ do
 
     case $choice in
         1)
-            # 安装 Git 和 Golang
-            if [[ -x "$(command -v apt-get)" ]]; then
-                sudo apt-get install git golang
-            elif [[ -x "$(command -v yum)" ]]; then
-                sudo yum install git golang
-            elif [[ -x "$(command -v dnf)" ]]; then
-                sudo dnf install git golang
-            elif [[ -x "$(command -v pkg)" ]]; then
-                sudo pkg install git golang
-            else
-                echo "Unsupported package manager."
-                exit 1
+            # 检查webBenchmark是否正在运行
+            if pgrep -f "webBenchmark" > /dev/null
+            then
+                echo "webBenchmark is already running. Please stop it first."
+                continue
             fi
 
-            # 安装 screen 避免任务被系统杀死
-            if [[ -x "$(command -v apt-get)" ]]; then
-                sudo apt-get install screen
-            elif [[ -x "$(command -v yum)" ]]; then
-                sudo yum install screen
-            elif [[ -x "$(command -v dnf)" ]]; then
-                sudo dnf install screen
-            elif [[ -x "$(command -v pkg)" ]]; then
-                sudo pkg install screen
-            else
-                echo "Unsupported package manager."
-                exit 1
-            fi
+            # 检查并安装 Git 和 Golang
+            for pkg in git golang screen
+            do
+                if ! command -v $pkg &> /dev/null
+                then
+                    if [[ -x "$(command -v apt-get)" ]]; then
+                        sudo apt-get install -y $pkg
+                    elif [[ -x "$(command -v yum)" ]]; then
+                        sudo yum install -y $pkg
+                    elif [[ -x "$(command -v dnf)" ]]; then
+                        sudo dnf install -y $pkg
+                    elif [[ -x "$(command -v pkg)" ]]; then
+                        sudo pkg install -y $pkg
+                    else
+                        echo "Unsupported package manager."
+                        exit 1
+                    fi
+                fi
+            done
 
             # 编译架构
             git clone https://github.com/maintell/webBenchmark.git
             cd webBenchmark
-            go build
+            if ! go build; then
+                echo "Failed to build webBenchmark"
+                exit 1
+            fi
 
             # 赋予权限
             chmod +x webBenchmark
@@ -62,18 +64,11 @@ do
             echo "webBenchmark 已经开始运行，您可以按 Ctrl+A+D 退出 screen 窗口任务，或者输入 2 停止运行 webBenchmark"
             ;;
         2)
-            # 安装psmisc
-            if [[ -x "$(command -v apt-get)" ]]; then
-                sudo apt-get install psmisc
-            elif [[ -x "$(command -v yum)" ]]; then
-                sudo yum install psmisc
-            elif [[ -x "$(command -v dnf)" ]]; then
-                sudo dnf install psmisc
-            elif [[ -x "$(command -v pkg)" ]]; then
-                sudo pkg install psmisc
-            else
-                echo "Unsupported package manager."
-                exit 1
+            # 检查webBenchmark是否正在运行
+            if ! pgrep -f "webBenchmark" > /dev/null
+            then
+                echo "webBenchmark is not running."
+                continue
             fi
 
             # 回到 screen 窗口任务
@@ -84,10 +79,9 @@ do
 
             # 获取所有名为"webBenchmark"的进程的PID，并逐个杀死
             pids=$(pgrep -f "webBenchmark")
-
             for pid in $pids
             do
-                killall -9 webBenchmark
+                kill -9 $pid
             done
 
             echo "webBenchmark 已经停止运行，您可以输入 3 退出脚本"
@@ -96,7 +90,7 @@ do
             # 退出脚本并回到root目录
             echo "感谢您使用一键命令脚本，再见！"
             running=false
-            cd
+            cd $current_dir
             break
             ;;
         *)
